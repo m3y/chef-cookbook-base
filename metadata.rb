95f5f30e6bd8ab8cd12d8b45b7c24b315213d6fc
Masaya Ozawa <ma3ya.ozw+github@gmail.com>
2013-09-10 02:23:02 +0900

    依存パッケージの追加

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'MIT'
 description      'Installs base packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'build-essential'
+depends          'vim'
+depends          'libevent-dev'
+depends          'libncurses5-dev'
